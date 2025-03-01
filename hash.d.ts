5983de8d472cf24063130fb849a65bef630e4fac
tarek ali <108217205+6tarek@users.noreply.github.com>
2025-03-01 14:04:37 +0200

    Delete hash.d.ts

--- hash.d.ts
+++
@@ -1,7 +0,0 @@
-import { JWT } from "./jwt";
-export declare class Hash extends JWT {
-    makePassword(password: string, key: string): string;
-    matchPassword(hash: string, password: string, key: string): boolean;
-    ID(): `${string}-${string}-${string}-${string}-${string}`;
-    salt(): string;
-}
